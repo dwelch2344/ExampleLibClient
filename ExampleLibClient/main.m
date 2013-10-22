3befb9022e65b85bedee826089b6add37e80c13c
Dave Welch <dave.welch@mokinetworks.com>
2013-10-22 09:23:46 -0600

    Initial Commit

---
+++ ExampleLibClient/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  ExampleLibClient
+//
+//  Created by Dave Welch on 10/22/13.
+//  Copyright (c) 2013 David Welch. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "DTWAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([DTWAppDelegate class]));
+    }
+}
